4e1f751fd74e395d43fabb77f41f053efdcd3569
sunzehui <501574022@qq.com>
2022-08-14 21:07:52 +0800

    chore(join-classes): 整理代码格式

--- src/views/classes/join-classes.vue
+++ src/views/classes/join-classes.vue
@@ -1,44 +1,33 @@
-<template>
-
-
-  <n-card title="搜索您的班级">
-    <n-input-group>
-      <n-input
-          v-model:value="searchInput"
-          :style="{ width: '50%' }"
-          placeholder="班级名称" size="large"/>
-      <n-button ghost size="large" type="primary" @click="filterClasses">
-        搜索
-      </n-button>
-    </n-input-group>
-  </n-card>
-
-  <n-card>
-    <n-data-table
-        :columns="columns"
-        :data="data"
-        :max-height="550"
-
-    />
-  </n-card>
-</template>
-
 <script lang="ts" setup>
-import type {DataTableColumns} from 'naive-ui'
-import {NButton} from 'naive-ui'
+import {DataTableColumn, NButton} from 'naive-ui'
 import {ApiFindClassesList, ApiJoinClass} from "@/apis/classes";
 import ClassesAction from "@/components/classes-action";
 import {ClassesResult} from "@/types/api-classes";
 import {get, isEmpty} from "lodash";
 
 
-type Classes = {
-  id: number,
-  name: string,
-  create_time: string,
+// 列表搜索
+const searchInput = ref("")
+const originData = ref<ClassesResult[]>([]);
+// 展示过滤后的data
+const sanitizedData = ref<ClassesResult[]>([]);
+
+const filterClasses = () => {
+  const oData = unref(originData)
+  if (isEmpty(oData)) return;
+  sanitizedData.value = oData.filter(c => c.name.includes(unref(searchInput)))
 }
 
-const createColumns = (row): DataTableColumns<Classes> => {
+const loadData = async () => {
+  const result = await ApiFindClassesList()
+  originData.value = result.data
+  sanitizedData.value = result.data;
+}
+
+watchEffect(() => loadData());
+
+
+const createColumns = (): (DataTableColumn | { render: (row: any) => any })[] => {
   return [
     {
       title: 'No',
@@ -56,7 +45,9 @@ const createColumns = (row): DataTableColumns<Classes> => {
       title: 'Action',
       key: 'actions',
       render(record: ClassesResult) {
-        const clickHandler = () => row.joinClasses(record)
+        const clickHandler = async () => {
+          return await ApiJoinClass(record.id).then(loadData);
+        }
         return h(
             ClassesAction,
             {
@@ -69,32 +60,27 @@ const createColumns = (row): DataTableColumns<Classes> => {
   ]
 }
 
-const searchInput = ref("")
-const filterClasses = () => {
-  const originData: ClassesResult[] = get(unref(res), 'data') || [];
-  console.log(res);
-  if (isEmpty(originData)) return;
-  data.value = originData.filter(c => c.name.includes(unref(searchInput)))
-  console.log(data.value)
-  console.log(searchInput.value);
-}
-const res = ref(null);
-const data = ref<ClassesResult[]>([]);
-
-const loadData = async () => {
-  res.value = await ApiFindClassesList()
-  data.value = res.value.data;
-}
-
-watchEffect(() => loadData());
-
-const columns = createColumns({
-  joinClasses(row: Classes) {
-    return ApiJoinClass(row.id).then(loadData);
-  }
-})
+const columns = createColumns()
 </script>
 
-<style scoped>
+<template>
+  <n-card title="搜索您的班级">
+    <n-input-group>
+      <n-input
+          v-model:value="searchInput"
+          :style="{ width: '50%' }"
+          placeholder="班级名称" size="large"/>
+      <n-button ghost size="large" type="primary" @click="filterClasses">
+        搜索
+      </n-button>
+    </n-input-group>
+  </n-card>
 
-</style>
+  <n-card>
+    <n-data-table
+        :columns="columns"
+        :data="sanitizedData"
+        :max-height="550"
+    />
+  </n-card>
+</template>
